3b2f3767292853831b7bf04b4c403fd8daebaebf
egyamfi6 <egyamfi189@yahoo.com>
2025-03-18 22:08:58 -0400

    adding demo filese

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+
+setup(
+
+    name='Demo',
+
+    version='0.1',
+
+    description='A simple demonstration package',
+
+    url='https://github.com/egyamfi6/m09-demo-1',
+
+    author='Emmanuel Gyamfio',
+
+    author_email='gyamfi6@gmail.comm',
+
+    license='MIT',
+
+    packages=['demo']
+
+)
